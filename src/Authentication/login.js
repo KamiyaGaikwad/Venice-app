6f09fc4efe8dcad54d6d59f4069080aa0d92ddb4
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2022-04-04 20:47:05 +0530

    refactor:changed a tags to Link

--- src/Authentication/login.js
+++ src/Authentication/login.js
@@ -1,46 +1,12 @@
-import { useEffect } from "react";
-import logo from "../Images/venice-logo.png";
+import { Link } from "react-router-dom";
+import Nav from "../Components/Nav";
 import "./login.css";
 
 export default function Login() {
-  useEffect(() => {
-    document.title = "Venice | Login";
-  }, []);
+  
   return (
     <>
-      <nav className="nav">
-        <div className="left">
-          <a href="https://venice-app.netlify.app/" className="logo">
-            <img src={logo} alt="logo" />
-          </a>
-          <a
-            href="https://venice-app.netlify.app/#"
-            className="btn btn-link brand"
-          >
-            Venice
-          </a>
-        </div>
-        <div className="right">
-          <div className="input-group search">
-            <input type="text" placeholder="Search" />
-          </div>
-          <a href="https://venice-app.netlify.app/Authentication/login.html">
-            <button className="btn btn-primary">Login</button>
-          </a>
-          <div className="badge">
-            <a href="https://venice-app.netlify.app/wishManagement/wish.html">
-              <i className="fas fa-heart" />
-              <div className="badge-icon">0</div>
-            </a>
-          </div>
-          <div className="badge cart-icon">
-            <a href="https://venice-app.netlify.app/CartManagement/cart.html">
-              <i className="fas fa-shopping-cart" />
-              <div className="badge-icon">0</div>
-            </a>
-          </div>
-        </div>
-      </nav>
+      <Nav />
       <div className="white-box">
         <h2 className="pb">Login</h2>
         <div className="input-group">
@@ -52,9 +18,9 @@ export default function Login() {
           <input className="pwd" type="password" required />
         </div>
         <button className="btn btn-primary">Login</button>
-        <a href="https://venice-app.netlify.app/Authentication/signup.html">
+        <Link to="/signup" class="no-deco">
           <button className="btn btn-link">Or Signup</button>
-        </a>
+        </Link>
       </div>
     </>
   );
--- src/Authentication/signup.js
+++ src/Authentication/signup.js
@@ -1,46 +1,12 @@
-import { useEffect } from "react";
-import logo from "../Images/venice-logo.png";
+import { Link } from "react-router-dom";
+import Nav from "../Components/Nav";
 import "./login.css";
 
 export default function SignUp() {
-  useEffect(() => {
-    document.title = "Venice | SignUp";
-  }, []);
+  
   return (
     <>
-      <nav className="nav">
-        <div className="left">
-          <a href="https://venice-app.netlify.app/" className="logo">
-            <img src={logo} alt="logo" />
-          </a>
-          <a
-            href="https://venice-app.netlify.app/"
-            className="btn btn-link brand"
-          >
-            Venice
-          </a>
-        </div>
-        <div className="right">
-          <div className="input-group search">
-            <input type="text" placeholder="Search" />
-          </div>
-          <a href="https://venice-app.netlify.app/Authentication/login.html">
-            <button className="btn btn-primary">Login</button>
-          </a>
-          <div className="badge">
-            <a href="https://venice-app.netlify.app/wishManagement/wish.html">
-              <i className="fas fa-heart" />
-              <div className="badge-icon">0</div>
-            </a>
-          </div>
-          <div className="badge cart-icon">
-            <a href="https://venice-app.netlify.app/CartManagement/cart.html">
-              <i className="fas fa-shopping-cart" />
-              <div className="badge-icon">0</div>
-            </a>
-          </div>
-        </div>
-      </nav>
+      <Nav />
       <div className="white-box-signup">
         <h2 className="pb">SignUp</h2>
         <div className="input-group">
@@ -67,9 +33,9 @@ export default function SignUp() {
           <input className="pwd" type="password" required />
         </div>
         <button className="btn btn-primary">SignUp</button>
-        <a href="https://venice-app.netlify.app/Authentication/login.html">
+        <Link to="/login">
           <button className="btn btn-link">Or Login</button>
-        </a>
+        </Link>
       </div>
       <div></div>
     </>
